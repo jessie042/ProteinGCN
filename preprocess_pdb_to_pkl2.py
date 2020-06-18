4cbb203fb86a6242e6692be66fdd588470283366
jessie042 <43130330+jessie042@users.noreply.github.com>
2020-06-17 22:20:51 -0400

    Update preprocess_pdb_to_pkl2.py

--- preprocess_pdb_to_pkl2.py
+++ preprocess_pdb_to_pkl2.py
@@ -174,7 +174,7 @@ def processDirectory(directory, max_neighbors, savepath, protein_id_prop_file):
 			writer = csv.writer(file)
 			#global_target, local_targets = get_targets(directory, filename)
 			#writer.writerow((save_filename, global_target, local_targets))
-			writer.writerow((save_filename,0,[0]*404))
+			writer.writerow((save_filename,0,[0]*len_amino))
 
 
 if get_pkl_flies:
